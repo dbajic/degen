f6845cb3f3402f5ede26e1f69ec4a29673c30a39
Denis Bajic <dbajic@hawk.iit.edu>
2016-05-18 18:48:35 -0500

    Initial implementation of Algorithm 1

--- degen.py
+++ degen.py
@@ -1,37 +1,38 @@
 import numpy as np
 import itertools
 import random
+import sys
 from scipy import stats, misc
 
 def degenerate(i,n,k):
-    V,E = degenerate_wrapper(i,n,k,[0 for i in range(n)],set(),set())
+    V,E = degenerate_wrapper(i,n,k,[0 for i in range(n)],set(),set()) # Call wrapper function
     return V,E
 
 def degenerate_wrapper(i,n,k,d,V,E):
-    if i == n: # Are we at the last node?
-        V.add(n)
+    if i == n: # Are we at the last vertex?
+        V.add(n) # Add vertex to vertex set
     else:
         d[i-1] = restricted_binomial(n-i,min(n-i,k)) # Generate degree d_i for vertex v_i
-        degenerate_wrapper(i+1,n,k,d,V,E)
+        degenerate_wrapper(i+1,n,k,d,V,E) # Recursively call function
         combs = []
         for r in itertools.combinations(V,d[i-1]): # Generate all subsets of degree d_i from V
             combs.append(r)
-        S = random.sample(combs,1)[0] # Uniformly sample one element from combinations
-        E.add((i,S))
-        V.add(i)
+        S = random.sample(combs,1)[0] # Uniformly sample one combination of edges
+        E.add((i,S)) # Add corresponding edges to edge list
+        V.add(i) # Add vertex to vertex list
     return V,E
 
 def restricted_binomial(n,k):
     probs = []
     values = np.arange(k+1)
-    sum_p = sum(misc.comb(n,i) for i in range(k+1))
+    sum_p = sum(misc.comb(n,i) for i in range(k+1)) # Save computation time by calculating the sum ahead of time
     for v in values:
-        probs.append(misc.comb(n,v)/sum_p)
-    rbin = stats.rv_discrete(name='rbin', values=(values,probs)) 
-    sample = rbin.rvs()
+        probs.append(misc.comb(n,v)/sum_p) # Create probability density function
+    rbin = stats.rv_discrete(name='rbin', values=(values,probs))
+    sample = rbin.rvs() # Random sample from distribution
     return sample
 	
 if __name__ == "__main__":
-	V,E = degenerate(1,100,4)
-	G = [V, E]
+	V,E = degenerate(int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3]))
+	G = [V,E]
 	print(G)
